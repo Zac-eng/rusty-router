28a1008bfb77a7135a4f0903ca89d39e8ebb1935
Zac-eng <zack.abten@gmail.com>
2025-01-30 12:14:57 +0900

    init

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,15 @@ fn main() -> io::Result<()> {
 
     let (lan_tx, mut lan_rx) = construct_interface(&env::var("LAN_INTF").unwrap(), "LAN_FIRSTHOP_MAC")?;
     let (wan0_tx, mut wan0_rx) = construct_interface(&env::var("WAN0_INTF").unwrap(), "WAN0_FIRSTHOP_MAC")?;
-    let (wan1_tx, mut wan1_rx) = construct_interface(&env::var("WAN1_INTF").unwrap(), "WAN1_FIRSTHOP_MAC")?;
     let lan_tx_arc = Arc::new(Mutex::new(lan_tx));
     let arc0 = Arc::clone(&lan_tx_arc);
-    let arc1 = Arc::clone(&lan_tx_arc);
 
     let mut handles: Vec<JoinHandle<io::Result<()>>> = Vec::new();
     handles.push(thread::spawn(move || {
-        lan_thread_func(&mut lan_rx, &mut vec![wan0_tx, wan1_tx])
+        lan_thread_func(&mut lan_rx, &mut vec![wan0_tx])
     }));
     handles.push(thread::spawn(move || {
         wan_thread_func(&mut wan0_rx, arc0)
-    }));
-    handles.push(thread::spawn(move || {
-        wan_thread_func(&mut wan1_rx, arc1)
     }));
     for handle in handles {
         let _ = handle.join().unwrap()?;

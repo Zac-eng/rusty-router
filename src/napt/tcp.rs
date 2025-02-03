869175e258c15b0eed3674742a26ea99fc8ffc3c
Zac-eng <zack.abten@gmail.com>
2025-02-03 12:22:47 +0900

    saving

--- src/napt/tcp.rs
+++ src/napt/tcp.rs
@@ -18,9 +18,13 @@ impl NAPTer {
   pub fn translate_outgoing_tcp(&mut self, ip_packet: MutableIpv4Packet) -> Option<MutableIpv4Packet<'static>> {
     let mut new_ip_packet = MutableIpv4Packet::owned(ip_packet.packet().to_vec()).unwrap();
     let mut new_tcp_packet = MutableTcpPacket::owned(ip_packet.payload().to_vec()).unwrap();
+    let original_ip = new_ip_packet.get_source();
     let original_port = u16::from_be(new_tcp_packet.get_source());
     let mut translated_port = original_port;
-    while let Some(_) = self.tcp_map.get(&translated_port) {translated_port+=1;}
+    while let Some(local_dst) = self.tcp_map.get(&translated_port) {
+      if local_dst.0 == original_ip {break;}
+      translated_port+=1;
+    }
     self.tcp_map.insert(translated_port, (new_ip_packet.get_source(), original_port));
     new_ip_packet.set_source(self.self_ip);
     new_tcp_packet.set_source(translated_port);

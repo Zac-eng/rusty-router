54bec87d64f286c4d8760616d09362e0a9cdd3f9
Zac-eng <zack.abten@gmail.com>
2025-02-18 04:35:20 +0900

    added calculation for tcp checksum

--- src/napt/mod.rs
+++ src/napt/mod.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, net::Ipv4Addr};
 
-use pnet::packet::{ip::IpNextHeaderProtocols::{Icmp, Tcp, Udp}, ipv4::MutableIpv4Packet, Packet};
+use pnet::packet::{ip::IpNextHeaderProtocols::{Icmp, Tcp, Udp}, ipv4::{Ipv4Packet, MutableIpv4Packet}, tcp::MutableTcpPacket, Packet, MutablePacket};
 
 mod tcp;
 
@@ -40,36 +40,69 @@ impl NAPTer {
   }
 }
 
-pub fn calc_translated_ip_checksum(original_checksum: u16, original_ip: &Ipv4Addr, new_ip: &Ipv4Addr) -> u16 {
-  // let ip_header = &ip_packet.packet()[..ip_packet.get_header_length() as usize * 4];
-  let original_octets = original_ip.octets();
-  let new_octets = new_ip.octets();
-
-  let original_ip_words = [u16::from_be_bytes([original_octets[0], original_octets[1]]), u16::from_be_bytes([original_octets[2], original_octets[3]])];
-  let new_ip_words = [u16::from_be_bytes([new_octets[0], new_octets[1]]), u16::from_be_bytes([new_octets[2], new_octets[3]])];
-  let mut new_sum = original_checksum as u32 + !original_ip_words[0] as u32 + !original_ip_words[1] as u32 + 2 + new_ip_words[0] as u32 + new_ip_words[1] as u32;
-  while (new_sum >> 16) != 0 {
-    new_sum = (new_sum & 0xFFFF) + (new_sum >> 16);
-  }
-  return new_sum as u16
+pub fn calc_ip_checksum(ip_packet: &MutableIpv4Packet) -> u16 {
+  compute_checksum(&ip_packet.packet()[..20])
 }
 
-pub fn calc_translated_transport_checksum(
-  original_checksum: u16,
-  original_ip: &Ipv4Addr,
-  new_ip: &Ipv4Addr,
-  original_port: u16,
-  new_port: u16
-) -> u16 {
-  // let ip_header = &ip_packet.packet()[..ip_packet.get_header_length() as usize * 4];
-  let original_octets = original_ip.octets();
-  let new_octets = new_ip.octets();
+pub fn calc_tcp_checksum(ip_packet: &MutableIpv4Packet) -> u16 {
+  let mut tcp = MutableTcpPacket::owned(ip_packet.payload().to_vec()).unwrap();
+  tcp.set_checksum(0);
+  let mut tcp_checksum_buf: Vec<u8> = Vec::new();
+  tcp_checksum_buf.append(&mut ip_packet.packet()[12..20].to_vec());
+  let tcp_packet_len = tcp.packet().len();
+  tcp_checksum_buf.append(&mut vec![0u8, 6u8, (tcp_packet_len/256) as u8, (tcp_packet_len%256)as u8]);
+  tcp_checksum_buf.append(&mut tcp.packet().to_vec());
+  compute_checksum(&tcp_checksum_buf)
+}
 
-  let original_ip_words = [u16::from_be_bytes([original_octets[0], original_octets[1]]), u16::from_be_bytes([original_octets[2], original_octets[3]])];
-  let new_ip_words = [u16::from_be_bytes([new_octets[0], new_octets[1]]), u16::from_be_bytes([new_octets[2], new_octets[3]])];
-  let mut new_sum = original_checksum as u32 + !original_ip_words[0] as u32 + !original_ip_words[1] as u32 + !original_port as u32 + 3 + new_ip_words[0] as u32 + new_ip_words[1] as u32 + new_port as u32;
-  while (new_sum >> 16) != 0 {
-    new_sum = (new_sum & 0xFFFF) + (new_sum >> 16);
+// pub fn calc_translated_ip_checksum(original_checksum: u16, original_ip: &Ipv4Addr, new_ip: &Ipv4Addr) -> u16 {
+//   // let ip_header = &ip_packet.packet()[..ip_packet.get_header_length() as usize * 4];
+//   let original_octets = original_ip.octets();
+//   let new_octets = new_ip.octets();
+
+//   let original_ip_words = [u16::from_be_bytes([original_octets[0], original_octets[1]]), u16::from_be_bytes([original_octets[2], original_octets[3]])];
+//   let new_ip_words = [u16::from_be_bytes([new_octets[0], new_octets[1]]), u16::from_be_bytes([new_octets[2], new_octets[3]])];
+//   let mut new_sum = original_checksum as u32 + !original_ip_words[0] as u32 + !original_ip_words[1] as u32 + 2 + new_ip_words[0] as u32 + new_ip_words[1] as u32;
+//   while (new_sum >> 16) != 0 {
+//     new_sum = (new_sum & 0xFFFF) + (new_sum >> 16);
+//   }
+//   return new_sum as u16
+// }
+
+// pub fn calc_translated_transport_checksum(
+//   original_checksum: u16,
+//   original_ip: &Ipv4Addr,
+//   new_ip: &Ipv4Addr,
+//   original_port: u16,
+//   new_port: u16
+// ) -> u16 {
+//   // let ip_header = &ip_packet.packet()[..ip_packet.get_header_length() as usize * 4];
+//   let original_octets = original_ip.octets();
+//   let new_octets = new_ip.octets();
+
+//   let original_ip_words = [u16::from_be_bytes([original_octets[0], original_octets[1]]), u16::from_be_bytes([original_octets[2], original_octets[3]])];
+//   let new_ip_words = [u16::from_be_bytes([new_octets[0], new_octets[1]]), u16::from_be_bytes([new_octets[2], new_octets[3]])];
+//   let mut new_sum = original_checksum as u32 + !original_ip_words[0] as u32 + !original_ip_words[1] as u32 + !original_port as u32 + 3 + new_ip_words[0] as u32 + new_ip_words[1] as u32 + new_port as u32;
+//   while (new_sum >> 16) != 0 {
+//     new_sum = (new_sum & 0xFFFF) + (new_sum >> 16);
+//   }
+//   return new_sum as u16
+// }
+
+fn compute_checksum(mut data: &[u8]) -> u16 {
+  let mut sum: u32 = 0;
+
+  while data.len() >= 2 {
+      let word = ((data[0] as u16) << 8) | (data[1] as u16);
+      sum += word as u32;
+      data = &data[2..];
   }
-  return new_sum as u16
+  if !data.is_empty() {
+      sum += (data[0] as u32) << 8;
+  }
+  while (sum >> 16) != 0 {
+      sum = (sum & 0xFFFF) + (sum >> 16);
+  }
+
+  !(sum as u16)
 }
